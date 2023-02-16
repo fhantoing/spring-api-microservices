872f9349bfc10513edda32ad20191607e53bb143
Héctor Fabio Lame López <fhantoing@gmail.com>
2023-02-16 11:54:42 -0500

    Implementación CircuitBreaker Resilience4j
    Implementación CircuitBreaker Resilience4j

--- cliente-servicio/src/main/java/com/cliente/servicio/controlador/ClienteControlador.java
+++ cliente-servicio/src/main/java/com/cliente/servicio/controlador/ClienteControlador.java
@@ -3,6 +3,7 @@ package com.cliente.servicio.controlador;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cliente.servicio.entidades.Cliente;
 import com.cliente.servicio.modelo.Direccion;
 import com.cliente.servicio.servicio.ClienteServicio;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 @RequestMapping("/cliente")
@@ -50,6 +53,7 @@ public class ClienteControlador {
 		return ResponseEntity.ok(nuevoCliente);
 	}
 
+	@CircuitBreaker(name = "direccionCB", fallbackMethod = "fallbackListarDirecciones")
 	@GetMapping("/direccion/{clienteId}")
 	public ResponseEntity<List<Direccion>> listarDirecciones(@PathVariable("clienteId") int id) {
 
@@ -63,12 +67,27 @@ public class ClienteControlador {
 		return ResponseEntity.ok(direcciones);
 	}
 
+	@CircuitBreaker(name = "direccionCB", fallbackMethod = "fallbackGuardarDireccion")
 	@PostMapping("/direccion/{clienteId}")
 	public ResponseEntity<Direccion> guardarDireccion(@PathVariable("clienteId") int id,
 			@RequestBody Direccion direccion) {
 
 		Direccion nuevaDireccion = clienteServicio.save(id, direccion);
 		return ResponseEntity.ok(nuevaDireccion);
+	}
+
+	@SuppressWarnings({ "unused", "rawtypes" })
+	private ResponseEntity<List<Direccion>> fallbackListarDirecciones(@PathVariable("clienteId") int id,
+			RuntimeException ex) {
+		return new ResponseEntity("Ha ocurrido un inconveniente al consultar las direcciones del Cliente: " + id,
+				HttpStatus.OK);
+	}
+
+	@SuppressWarnings({ "unused", "rawtypes" })
+	private ResponseEntity<List<Direccion>> fallbackGuardarDireccion(@PathVariable("clienteId") int id,
+			@RequestBody Direccion direccion, RuntimeException ex) {
+		return new ResponseEntity("Ha ocurrido un inconveniente al guardar las direccion del Cliente: " + id,
+				HttpStatus.OK);
 	}
 
 }
